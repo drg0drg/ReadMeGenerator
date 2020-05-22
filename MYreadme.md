121c94a5c64e962b9ff1dcbe799e455c19a0560b
drg0drg <60710786+drg0drg@users.noreply.github.com>
2020-05-22 11:43:37 +0100

    readMeText variable that assembles the content of the readme from user input completed

---
+++ MYreadme.md
@@ -0,0 +1,27 @@
+## fdsgf 
+
+# Description 
+fdg sdfg sfdg sdfgsdfg sfdg sdfg sfdg sdfg sdfg sdfg sfdg sfg sfg 
+
+# Table of Contents
+sdfg sdfg sfg 
+
+# License
+sdfg sdfg sfg 
+
+# Teststuyint 
+
+# Contribution
+tyinti tuntitn 
+
+# Installation
+tuintuntimt
+
+# Usage
+tyunituntu itunitunt tiunt
+
+ #Author
+drg
+
+# Contact
+drg
--- index.js
+++ index.js
@@ -2,59 +2,75 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
 
-
 const writeFileAsync = util.promisify(fs.writeFile);
 
-function generateReadMe (){
-  const userInput = inquirer.prompt([{
-    message: "Please enter your name",
-    name: "name"
-  },
-  {
-    message: "Please enter your GitHub username",
-    name: "GitHubUserName"
-  },
-  {
-    message: "Please enter your project title",
-    name: "ProjectTitle"
-  },
-  {
-    message: "Please enter your project repo name on GitHub",
-    name: "ProjectRepo"
-  },
-  {
-    message: "Please enter your project description",
-    name: "ProjectDescription"
-  },
-  {
-    message: "Please enter your project table of contents",
-    name: "ProjectContents"
-  },
-  {
-    message: "Please enter your project installation requirements",
-    name: "ProjectInstall"
-  },
-  {
-    message: "Please enter your project can be used",
-    name: "ProjectUse"
-  },
-  {
-    message: "Please enter contributing info for your project",
-    name: "ProjectContributing"
-  },
-  {
-    message: "Please enter what tests has your project passed",
-    name: "ProjectTest"
-  },
-  {
-    message: "Please enter your contact for this project",
-    name: "ProjectContact"
-  },
-])
+async function generateReadMe() {
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Please enter your name",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Please enter your GitHub username",
+        name: "userName",
+      },
+      {
+        type: "input",
+        message: "Please enter your project title",
+        name: "projectTitle",
+      },
+      {
+        type: "input",
+        message: "Please enter your project repo name on GitHub",
+        name: "projectRepo",
+      },
+      {
+        type: "input",
+        message: "Please enter your project description",
+        name: "projectDescription",
+      },
+      {
+        type: "input",
+        message: "Please enter your project table of contents",
+        name: "ProjectContents",
+      },
+      {
+        type: "input",
+        message: "Please enter your project installation requirements",
+        name: "ProjectInstall",
+      },
+      {
+        type: "input",
+        message: "Please enter your project can be used",
+        name: "ProjectUse",
+      },
+      {
+        type: "input",
+        message: "Please enter contributing info for your project",
+        name: "ProjectContributing",
+      },
+      {
+        type: "input",
+        message: "Please enter what tests has your project passed",
+        name: "ProjectTest",
+      },
+      {
+        type: "input",
+        message: "Please enter your contact for this project",
+        name: "ProjectContact",
+      },
+    ]);
 
-const readMeText = `${userInput.ProjectTitle} Description:${userInput.ProjectDescription}`;
-const myReadMe = writeFileAsync("readme.md", readMeText);
+    console.log(userInput);
 
+    const readMeText = `## ${userInput.projectTitle}\n\n# Description \n${userInput.projectDescription}\n\n# Table of Contents\n${userInput.ProjectContents}\n\n# License\n${userInput.ProjectContents}\n\n# Tests\n${userInput.ProjectTest}\n\n# Contribution\n${userInput.ProjectContributing}\n\n# Installation\n${userInput.ProjectInstall}\n\n# Usage\n${userInput.ProjectUse}\n\n #Author\n${userInput.name}\n\n# Contact\n${userInput.name}`;
+    writeFileAsync("MYreadme.md", readMeText);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-generateReadMe ();
+generateReadMe();
